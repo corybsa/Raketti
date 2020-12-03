8c1a3ef9210bb30989cf4a53f4aab98b10db13d0
Cory Sandlin <corybsa@gmail.com>
2020-12-03 09:03:09 -0500

    login screen

--- Raketti/Client/Program.cs
+++ Raketti/Client/Program.cs
@@ -25,6 +25,7 @@ namespace Raketti.Client
 			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 			builder.Services.AddScoped<IUserService, UserService>();
 			builder.Services.AddScoped<IAuthService, AuthService>();
+			builder.Services.AddScoped<BrowserService>();
 			builder.Services.AddOptions();
 			builder.Services.AddAuthorizationCore();
 			builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
---
+++ Raketti/Client/Services/BrowserService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Raketti.Client.Services
+{
+	public class BrowserService
+	{
+		private readonly IJSRuntime _js;
+
+		public BrowserService(IJSRuntime js)
+		{
+			_js = js;
+		}
+
+		public async Task<BrowserDimensions> GetDimensions()
+		{
+			return await _js.InvokeAsync<BrowserDimensions>("getDimensions");
+		}
+	}
+
+	public class BrowserDimensions
+	{
+		public int Width { get; set; }
+		public int Height { get; set; }
+	}
+}
